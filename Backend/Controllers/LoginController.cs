11ce987ab349f05c589532b6c7cb0040ce8f7a6f
Ivanna <Ivanna@DESKTOP-SROO9RR>
2019-02-27 18:11:09 +0200

    Changed login controller

--- Backend/Controllers/LoginController.cs
+++ Backend/Controllers/LoginController.cs
@@ -4,26 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FreelanceLand.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Options;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Backend.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class LoginController : Controller
     {
         EFGenericRepository<User> userRepository = new EFGenericRepository<User>(new ApplicationContext());
 
-        [HttpGet]
-        public bool ValidateUser(string login, string password)
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult ValidateUser(string login, string password)
         {
-            bool authentication = false;
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest(new { message = "Login or password is incorrect" });
+            }
             IEnumerable<User> users = userRepository.Get(u => u.Login == login);
             if (users.Count(u => u.Password == password) > 0)
             {
-                authentication = true;
+                return Ok();
             }
-            return authentication;
+
+            return BadRequest(new { message = "Login or password is incorect" });
         }
     }
 }
