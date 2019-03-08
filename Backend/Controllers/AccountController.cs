511de80ee52c972f2f8f178d3a882d741773ac30
Andrii Veremeienko <illidan00000@gmail.com>
2019-03-08 16:25:43 +0200

    Created branch

--- Backend/Controllers/AccountController.cs
+++ Backend/Controllers/AccountController.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using FreelanceLand.Models;
+﻿using FreelanceLand.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Backend.Controllers
 {
